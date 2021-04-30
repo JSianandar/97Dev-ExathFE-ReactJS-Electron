827d77067daea1578b8db589b3bdbbef84692539
ExtGmrJasonZ <43334472+ExtGmrJasonZ@users.noreply.github.com>
2021-04-30 18:04:17 +0700

    Settings Page (Need Padding)

--- public/electron.js
+++ public/electron.js
@@ -18,7 +18,6 @@ function createWindow() {
         ? "http://localhost:3000"
         : `file://${path.join(__dirname, "../build/index.html")}`
     );
-    mainWindow.webContents.openDevTools();
     mainWindow.on("closed", () => (mainWindow = null));
     
 }
--- src/Settings.js
+++ src/Settings.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import './css/Settings.css';
 import {Link} from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+
+import discord_logo from './assets/icons/discord_logo.png';
+import user_logo from './assets/icons/user_logo.png';
+import password_logo from './assets/icons/password_logo.png';
+import profile_logo from './assets/icons/profile_logo.png';
+import ruler_logo from './assets/icons/ruler_logo.png';
+
 
 class Settings extends React.Component{
 	constructor(){
@@ -27,6 +35,56 @@ class Settings extends React.Component{
 					<div className="setup-wrapper row">
 						<div className="col-2 pt-2 pr-5">
 							<h1 className="setup text-center">Setup</h1>
+						</div>
+					</div>
+
+					<div className="row mx-auto">
+						<div className="setup-button-wrapper col-6 pt-2 d-flex">
+							<img className="discord_icon" src={discord_logo} />
+							<p className="heading my-auto">Discord Webhook</p>
+						</div>
+						<div className="col-1"></div>
+						<div className="test-button-wrapper col-1">
+							<p className="heading my-auto text-center pt-1">Test</p>
+						</div>
+					</div>
+
+					<div className="quick-task-wrapper row">
+						<div className="col-3 pt-2 pr-5">
+							<h1 className="quick-task text-center">Quick Task</h1>
+						</div>
+					</div>
+					<div className="row mx-auto pt-2">
+						<div className="setup-button-wrapper col-6 pt-2 d-flex">
+							<img className="discord_icon" src={profile_logo} />
+							<p className="heading my-auto">Choose Profile</p>
+						</div>
+					</div>
+
+					<div className="row mx-auto pt-2">
+						<div className="setup-button-wrapper col-6 pt-2 d-flex">
+							<img className="discord_icon" src={ruler_logo} />
+							<p className="heading my-auto">Preferred Size</p>
+						</div>
+					</div>
+					<div className="row mx-auto pt-4">
+						<div className="col-4 quick-task-button-wrapper">
+							<img className="discord_icon" src={user_logo} />
+						</div>
+						<div className="col-1"></div>
+						<div className="col-4 quick-task-button-wrapper">
+							<img className="discord_icon" src={password_logo} />
+						</div>
+					</div>
+					<div className="updates-wrapper row">
+						<div className="col-2 pt-2 pr-4">
+							<h1 className="update text-center">Updates</h1>
+						</div>
+					</div>
+
+					<div className="row mx-auto">
+						<div className="cfu-button-wrapper pt-1">
+							<h1 className="heading text-center">Check for Updates</h1>
 						</div>
 					</div>
 				</div>
