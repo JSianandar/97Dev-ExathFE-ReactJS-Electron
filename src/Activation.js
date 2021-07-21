68c27960594c390496216100ac575560dd3087f0
Alifio Rasendriya Rasyid <official.alifio@gmail.com>
2021-07-21 22:14:33 +0700

    Fix bug on Activation page.
     - This page can now handle multiple invalid calls.

--- src/Activation.js
+++ src/Activation.js
@@ -7,144 +7,72 @@ import axios from 'axios';
 
 
 class Activation extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-        key: '',
-        mode: 'true',
-
+    constructor(props){
+        super(props)
+        this.state = {
+            key: '',
+        }
     }
-    this.trueKeyActivate = this.trueKeyActivate.bind(this)
-    this.wrongKeyActivate = this.wrongKeyActivate.bind(this)
-  }
 
-  handleChange = event => {
-	this.setState({ [event.target.name]: event.target.value });
-  }
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    }
 
-  handleSubmit = event => {
-    event.preventDefault();
-    this.getActivationKey()
-  }
+    handleSubmit = event => {
+        event.preventDefault();
+        this.getActivationKey()
+    }
 
-  trueKeyActivate(){
-        this.setState({mode: 'true'})
-  }
-
-  wrongKeyActivate(){
-        this.setState({mode: 'false'})
-        console.log('false')
-
-  }
-
-  getActivationKey = () =>{
-    axios.get(`http://exath.io/api/authenticate?key=${this.state.key}`)
-    .then(res => {
-        this.setState({
-            key: res.data
-
+    getActivationKey = () =>{
+        axios.get(`http://exath.io/api/authenticate?key=${this.state.key}`)
+        .then(res => {
+            window.location.href = '/task';
+        }, error => {
+            // If key is invalid or something went wrong, change the input text value.
+            document.getElementById('appKeyInput').value = 'INVALID KEY, Forbidden'
         })
-        console.log(res);
-        console.log(res.data);
-        window.location.href = '/task';
-    },error => {
-        this.setState({
-            mode: 'false'
-        })
-    })
-  }
+    }
+    
+    componentDidMount(){}
 
-
-  componentDidMount(){
-  }
-  
-
-  render(){
+    render(){
         return(
             <div>
-                {this.state.mode == 'true' &&  <div className="activation-container">
-	                    <div className="row pt-4">
-                            <div className="exath-activation mx-auto"></div>   
-                        </div>
-                        <div className="row pt-3">
-                            <div className="col-12">
-                                <form>
-                                    <input 
+                <div className="activation-container">
+                    <div className="row pt-4">
+                        <div className="exath-activation mx-auto"></div>   
+                    </div>
+                    <div className="row pt-3">
+                        <div className="col-12">
+                            <form>
+                                <input
+                                    id="appKeyInput"
                                     type="text"
-                                    required
                                     name= "key"
                                     onChange = {this.handleChange}
                                     value = {this.state.key}
                                     placeholder = "Enter your key"
                                     className="key_input"
-                                    />
-                                </form>
-                            </div>
-                        </div>
-
-                        <div className="row pt-5">
-                            <div className="col-3 ml-4"></div>
-                            <Link className="col-2 ml-5 button" style={{ textDecoration: 'none' }}>
-                                <h1 className="text-center">Close</h1>
-                            </Link>
-                
-                            <Link onClick={this.handleSubmit} className="col-2 ml-5 button" style={{ textDecoration: 'none' }}>
-                                <h1 className="text-center">Activate</h1>
-                            </Link>
-                            <div className="col-3"></div>
-                        </div>
-                    </div>}
-
-
-
-                    {this.state.mode == 'false' && 
-                    <div className="activation-container">
-	                    <div className="row pt-4">
-                            <div className="exath-activation mx-auto"></div>   
-                        </div>
-                        <div className="row pt-3">
-                            <div className="col-12">
-                                <form>
-                                    <input 
-                                    type="text"
                                     required
-                                    placeholder = "INVALID KEY, Forbidden"
-
-                                    className="key_input"
-                                    />
-                                </form>
-                            </div>
+                                />
+                            </form>
                         </div>
+                    </div>
 
-                        
-
-
-
-                        <div className="row pt-5">
-                            <div className="col-3 ml-4"></div>
-                            <Link className="col-2 ml-5 button" onClick = {this.trueKeyActivate} style={{ textDecoration: 'none' }}>
-                                <h1 className="text-center">Close</h1>
-                            </Link>
-                
-                            <Link to="/task" className="col-2 ml-5 button" style={{ textDecoration: 'none' }}>
-                                <h1 className="text-center">Activate</h1>
-                            </Link>
-                            <div className="col-3"></div>
-                        </div>
-                        
-{/*                        <div className="row pt-3">
-                            <div className="col-6"></div>
-                            <div className="col-4 ml-4">
-                                <Alert variant = 'danger' style={{width: '135px'}}>
-                                    INVALID KEY 
-                                </Alert>
-                            </div>
-                        </div>
-*/}
-                    </div>}
+                    <div className="row pt-5">
+                        <div className="col-3 ml-4"></div>
+                        <Link className="col-2 ml-5 button" style={{ textDecoration: 'none' }}>
+                            <h1 className="text-center">Close</h1>
+                        </Link>
+            
+                        <Link onClick={this.handleSubmit} className="col-2 ml-5 button" style={{ textDecoration: 'none' }}>
+                            <h1 className="text-center">Activate</h1>
+                        </Link>
+                        <div className="col-3"></div>
+                    </div>
                 </div>
-
-	    );
+            </div>
+        );
     }
 }
 
