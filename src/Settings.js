6c17276de2467c8ff9cf159c3a07f603000e4c1a
ExtGmrJasonZ <43334472+JSianandar@users.noreply.github.com>
2021-07-26 15:59:30 +0700

    added title bar to other pages

--- src/Settings.js
+++ src/Settings.js
@@ -66,7 +66,6 @@ class Settings extends React.Component{
 	componentDidUpdate(prevprop){
 		if(prevprop != this.props){
 			this.setState({
-				
 			})
 		}
 	}
@@ -100,8 +99,8 @@ class Settings extends React.Component{
 	render(){
 		return(
 			<div className="settings">
-				{/*<TitleBar/>*/}
-				<div className="settings-container pt-2">
+				<TitleBar/>
+				<div className="settings-container pt-0">
 					<div className="page-wrapper mx-auto">
 						<div className="title-wrapper row">
 							<div className="col-2 pt-3 ml-1">
